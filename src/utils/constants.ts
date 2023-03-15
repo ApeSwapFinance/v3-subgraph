10712c45093f36c88dd8bd24d204601fafb31d1c
Professor S <84548711+Prof-SD@users.noreply.github.com>
2023-03-15 00:03:34 -0700

    Added BSC

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+export const FACTORY_ADDRESS = '0x5a6511F7CD85e5bCaad3D72B0ed22AF163363A63'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
